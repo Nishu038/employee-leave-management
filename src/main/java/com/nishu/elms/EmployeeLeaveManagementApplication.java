cb3cc30ef7768395caac085d2fbce0f2b79f59e1
Nishigandha Malvade <nishigandhamalvade@gmail.com>
2026-07-14 15:42:16 +0530

    Add BaseEntity with auditing fields

--- src/main/java/com/nishu/elms/EmployeeLeaveManagementApplication.java
+++ src/main/java/com/nishu/elms/EmployeeLeaveManagementApplication.java
@@ -2,8 +2,10 @@ package com.nishu.elms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EmployeeLeaveManagementApplication {
 
 	public static void main(String[] args) {
